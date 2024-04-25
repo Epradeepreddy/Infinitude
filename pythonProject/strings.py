2f506221646b09ccdb98c153124fc7b7159f34a2
Epradeepreddy <164894937+Epradeepreddy@users.noreply.github.com>
2024-04-25 15:15:14 +0530

    Delete pythonProject directory

--- pythonProject/strings.py
+++
@@ -1,8 +0,0 @@
-name = "pradeep"
-print(name[2])
-
-x=input("enter the input value\n")
-print(x)
-
-print(name.upper())
-print(x.lower())
--- pythonProject/tuple.py
+++
@@ -1,8 +0,0 @@
-mytuple=(10,20,10,40,50)
-
-p=mytuple.index(20)
-print(p)
-
-r=mytuple.count(10)
-print(r)
-
